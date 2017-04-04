c7d73b04c21c4a31acfe3b7877adb8b45b5d3769
Juan Vasquez <juanfevasquez@iMac-de-Juan.local>
2017-04-03 23:04:01 -0500

    videos 1 and 2

--- basics/01-intro/start/js/main.js
+++ basics/01-intro/start/js/main.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
 
-	
+	//Init scrollmagic to tell the browser that scrolling will trigger certain behavior
+    var controller = new ScrollMagic.Controller();
+    //Scroll animations are always define within a Scene
+
+    //Build a Scene
+    var ourScene = new ScrollMagic.Scene({
+        triggerElement: '#project01 img',
+        duration: '100%', //100% this means 100% of the viewport height
+        triggerHook: 1  //1 means bottom, 0 top
+    })
+    .setClassToggle('#project01', 'fade-in') // this class contains the visual changes
+    .addIndicators()  //this requires a plugin
+    .addTo(controller);
 
 });
 
